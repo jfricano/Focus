1ef805a5c5167f6c057ffd9060eeca0d6061ebbc
Jason Fricano <jason.fricano@gmail.com>
2020-08-24 19:32:58 +0800

    commit on master

---
+++ main.js
@@ -0,0 +1,31 @@
+// old code using sans jQuery syntax
+// const contents = document.getElementById('content');
+// contents.parentNode.removeChild(contents);
+
+// ***** pull relevant elements from fb, youtube ****** 
+const contents = $('#content');
+const parent = contents.parent();
+
+// *********** remove main container ************ 
+contents.remove();
+
+// *********** define new elements **************
+// image
+const image = document.createElement('img');
+image.src =
+	'https://www.shadygrovefertility.com/blog/wp-content/uploads/2014/07/hang-in-there.jpg';
+
+// background
+const background = document.createElement('div');
+background.id = 'background';
+
+// text
+const textContainer = document.createElement('div');
+const text = document.createTextNode('Get back to building your dreams');
+textContainer.id = 'beautText';
+
+// assemble new elements
+parent.append(background);
+background.append(textContainer);
+textContainer.append(text);
+background.append(image);
